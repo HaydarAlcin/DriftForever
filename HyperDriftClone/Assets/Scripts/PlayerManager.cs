f7fd82f64b9de2c0c62f95d43dacb5ff1a4dd3b9
Haydar Alçin <hydralcin@gmail.com>
2022-09-12 00:46:16 +0300

    v0.4

--- HyperDriftClone/Assets/Scripts/PlayerManager.cs
+++ HyperDriftClone/Assets/Scripts/PlayerManager.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] GameObject currentRoad;
-    [SerializeField] Transform  nextRoad1;
-    [SerializeField] Transform  nextRoad2;
+    [SerializeField] Transform nextRoad1;
+    [SerializeField] Transform nextRoad2;
+    [SerializeField] Transform nextRoad3;
+    [SerializeField] Transform nextRoad4;
+    [SerializeField] Transform nextRoad5;
+    [SerializeField] Transform nextRoad6;
 
 
 
     public float changeRoad=-1;
 
+    public float dondurme;
 
     public bool justOne;
     void Start()
@@ -19,10 +24,7 @@ public class PlayerManager : MonoBehaviour
         justOne = true;
     }
 
-    void Update()
-    {
-        
-    }
+    
 
 
     private void OnTriggerStay(Collider other)
@@ -33,7 +35,7 @@ public class PlayerManager : MonoBehaviour
         {
             
 
-            changeRoad = Random.Range(0, 2);
+            changeRoad = Random.Range(0,3);
 
             switch (changeRoad)
             {
@@ -48,17 +50,56 @@ public class PlayerManager : MonoBehaviour
                     //nextRoad2.transform.position = currentRoad.transform.GetChild(2).GetComponent<Transform>().position;
                     Instantiate(nextRoad2, currentRoad.transform.GetChild(2).GetComponent<Transform>().position, Quaternion.identity);
                     break;
+
+                case 2:
+                    //nextRoad2.transform.position = new Vector3(0, 0, currentRoad.transform.GetChild(0).GetComponent<Renderer>().bounds.size.z/5);
+                    //nextRoad2.transform.position = currentRoad.transform.GetChild(2).GetComponent<Transform>().position;
+                    Instantiate(nextRoad3, currentRoad.transform.GetChild(2).GetComponent<Transform>().position, Quaternion.identity);
+                    break;
             }
 
             justOne = false;
         }
 
-        if (justOne==true)
+        if ( other.tag=="YanYol" && justOne==true)
         {
+
+            changeRoad = Random.Range(0,2);
+              
+            switch (changeRoad)
+            {
+                case 0:
+                    
+                    Instantiate(nextRoad4,currentRoad.transform.GetChild(2).GetComponent<Transform>().position, Quaternion.Inverse(Quaternion.Euler(0, -90, 0)));
+                    
+                    break;
+
+                case 1:
+                    Instantiate(nextRoad5, currentRoad.transform.GetChild(2).GetComponent<Transform>().position, Quaternion.Inverse(Quaternion.Euler(0, 0, 0)));
+                    break;
+            }    
+            //Instantiate(nextRoad1,currentRoad.transform.GetChild(2).gameObject.transform.position, Quaternion.identity);
             
-            
-                Instantiate(nextRoad1,currentRoad.transform.GetChild(2).gameObject.transform.position, Quaternion.identity);
-            
+            justOne = false;
+        }
+
+
+        if (other.tag == "YanYolLeft" && justOne == true)
+        {
+
+            changeRoad = Random.Range(0, 2);
+
+            switch (changeRoad)
+            {
+                case 0:
+                    Instantiate(nextRoad4, currentRoad.transform.GetChild(2).gameObject.transform.position, Quaternion.Inverse(Quaternion.Euler(0, 90, 0)));
+                    break;
+
+                case 1:
+                    Instantiate(nextRoad6, currentRoad.transform.GetChild(2).gameObject.transform.position, Quaternion.Inverse(Quaternion.Euler(0, 0, 0)));
+                    break;
+            }
+            //Instantiate(nextRoad1,currentRoad.transform.GetChild(2).gameObject.transform.position, Quaternion.identity);
 
             justOne = false;
         }
